f38a19f6da6adf696dc2d72c73d3f58f4cee3a56
skonstantis <skonstantis@uth.gr>
2024-04-26 12:09:21 +0300

    initializing app

---
+++ README.md
@@ -0,0 +1,2 @@
+This is a React app showing all 112 alerts that were sent to Greece since the beginning of the service. 
+The alerts are updated manually perodically, since there is no api provided
---
+++ main/src/App.jsx
@@ -0,0 +1,13 @@
+/* Author: Sotiris Konstantis */
+
+import { useState } from "react";
+
+import "./app.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      
+    </div>
+  );
+}
